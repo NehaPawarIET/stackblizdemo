2bcaa275fe01de4518c772f5fab81faebfc7e39e
NehaPawarIET <51132697+NehaPawarIET@users.noreply.github.com>
2022-10-05 23:17:39 +0530

    1 file modified, 4 added, 4 deleted

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,4 @@
 import { Component } from '@angular/core';
 
-@Component({
-  selector: 'my-app',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css'],
-})
+@Component({ selector: 'app', templateUrl: 'app.component.html' })
 export class AppComponent {}
